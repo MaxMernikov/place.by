5f84599623c3a490d544b7c95e497018e2bafbf7
max mernikov <maxmernikov@gmail.com>
2014-01-12 22:07:03 +0300

    sd

--- db/migrate/20131230141839_change_name_in_category.rb
+++
@@ -1,9 +0,0 @@
-class ChangeNameInCategory < ActiveRecord::Migration
-  def change
-    change_table :categories do |t|
-      t.remove :category_id
-      t.string :ancestry
-      t.index :ancestry
-    end
-  end
-end
