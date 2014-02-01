9269854430a0bde6cf0535948d0df4dea5a32cf2
max mernikov <maxmernikov@gmail.com>
2014-02-02 01:01:25 +0300

    fix alert

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 set :application, 'plce.by'
 set :repo_url, 'git@github.com:MaxMernikov/place.by.git'
 
-set :deploy_to, '/www/plce.by'
 set :scm, :git
 set :user, 'root'
 set :ssh_options, { :forward_agent => true }
 # set :rails_env, 'staging'
 set :deploy_via, :copy
 
-set :keep_releases, 5
+set :keep_releases, 2
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
 
 set :bundle_gemfile, -> { release_path.join('Gemfile') }
 set :bundle_dir, -> { shared_path.join('bundle') }
@@ -21,21 +21,13 @@ set :bundle_roles, :all
 
 namespace :deploy do
 
-  desc 'Restart application'
+  desc "Restart application"
   task :restart do
-    on roles(:app) do
-      within release_path do
-        # with rails_env: fetch(:stage) do
-          # rake 'resque:restart_workers'
-          # execute "thin restart -p 3000 -s1 -e production"
-        # end
-      end
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join("tmp/restart.txt")
     end
-
-    # on roles(:app), in: :sequence, wait: 5 do
-    #   within release_path do
-    #     execute "thin restart -p 3000 -s1 -e production"
-    #   end
-    # end
   end
+
+  # after :finishing, "deploy:cleanup"
+
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
 set :stage, :production
 set :branch, 'master'
-set :deploy_to, '/www/plce.by/'
+set :deploy_to, '~/www/plce.by/'
 set :rails_env, 'production'
 
-role :app, %w{root@162.243.33.247}
-role :web, %w{root@162.243.33.247}
-role :db,  %w{root@162.243.33.247}
+role :app, %w{root@95.85.12.211}
+role :web, %w{root@95.85.12.211}
+role :db,  %w{root@95.85.12.211}
 
-server '162.243.33.247', user: 'root', roles: %w{app web db}
+server '95.85.12.211', user: 'root', roles: %w{app web db}
